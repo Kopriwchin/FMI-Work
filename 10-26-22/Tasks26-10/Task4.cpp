85e5768f0c1f4ecc7910cd415c93d11ca3ac7a05
Kopriwchin <iliqn1234a@gmail.com>
2022-10-26 16:01:47 +0300

    Add task 4

---
+++ 10-26-22/Tasks26-10/Task4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+
+    int low, high, i;
+    bool is_prime = true;
+
+    std::cin >> low >> high;
+
+    while (low < high) {
+        is_prime = true;
+
+        if (low == 0 || low == 1) {
+            is_prime = false;
+        }
+
+        for (i = 2; i <= low / 2; ++i) {
+            if (low % i == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+
+        if (is_prime)
+            std::cout << low << ", ";
+
+        ++low;
+    }
+
+    return 0;
+}
