14a76b11b03a714f6dda1bf1676ad4fda8c1fe11
Kopriwchin <iliqn1234a@gmail.com>
2022-11-13 15:04:09 +0200

    Add task 2

---
+++ 11-13-22-Exam-Tasks/Task2.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int number)
+{
+	if (number == 2)
+	{
+		return true;
+	}
+
+	for (int i = 2; i <= number - 1; i++)
+	{
+		if (number % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int main()
+{
+	int n;
+	int lastNum;
+
+	cin >> n;
+
+	for (int i = 2; n != 1; i++)
+	{
+		lastNum = i;
+
+		if (isPrime(i))#include <iostream>
+
+using namespace std;
+
+bool isPrime(int number)
+{
+	if (number == 2)
+	{
+		return true;
+	}
+
+	for (int i = 2; i <= number - 1; i++)
+	{
+		if (number % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int main()
+{
+	int n;
+	int lastNum;
+
+	cin >> n;
+
+	for (int i = 2; n != 1; i++)
+	{
+		lastNum = i;
+
+		if (isPrime(i))
+		{
+			if (n % i == 0)
+			{
+				n /= i;
+				if (n == 1)
+				{ 
+					cout << i;
+					return 0;
+				}
+				cout << i << ".";
+				i--;
+			}
+		}
+	}
+}
+		{
+			if (n % i == 0)
+			{
+				n /= i;
+				if (n == 1)
+				{ 
+					cout << i;
+					return 0;
+				}
+				cout << i << ".";
+				i--;
+			}
+		}
+	}
+}
