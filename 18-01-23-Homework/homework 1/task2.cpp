b0484ad59cb4e8670997c014db4e87184fe655ad
Kopriwchin <iliqn1234a@gmail.com>
2023-01-11 00:32:04 +0200

    Refactor task 2

--- 18-01-23-Homework/homework 1/task2.cpp
+++ 18-01-23-Homework/homework 1/task2.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <string>
+
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
+using std::getline;
 
-void BruteForceDecryptCipher(char* cipherText)
+void BruteForceDecryptCipher(const char* cipherText)
 {
+	cout << endl;
+
+	int textLength = strlen(cipherText);
+
 	int startIndex = 65;
 	int endIndex = 90;
-	char* decryptedText = new char[1024];
+	char* decryptedText = new char[textLength];
 
-	if (strlen(cipherText) >= 1024)
+	if (textLength >= 1024)
 	{
 		cout << "Text too long!";
 		return;
@@ -17,9 +25,9 @@ void BruteForceDecryptCipher(char* cipherText)
 
 	for (size_t i = 0; i < 26; i++)
 	{
-		cout << "plain text for key " << i << ":- ";
+		cout << "plain text for key " << i << " :- ";
 
-		for (size_t j = 0; j < strlen(cipherText); j++)
+		for (size_t j = 0; j < textLength; j++)
 		{
 			if (cipherText[j] == ' ')
 			{
@@ -29,7 +37,7 @@ void BruteForceDecryptCipher(char* cipherText)
 
 			if (cipherText[j] < 65 || 95 < cipherText[j])
 			{
-				cout << "Wrong input!";
+				cout << "\nNext character " << "\'" << cipherText[j] << "\'" << " is invalid! " << "Only capital letters allowed!";
 				return;
 			}
 
@@ -45,22 +53,32 @@ void BruteForceDecryptCipher(char* cipherText)
 
 		cout << endl;
 	}
+
+	delete[] decryptedText;
 }
 
 int main()
 {
+	/* // Test example of the program
 	char input1[] = "FTUE UE MZ QJMYBXQ YQEEMSQ FA NQ QZODKBFQP";
 
-	char input2[] = { 'a', 'b', 'c' };
+	char input2[] = "AcBC";
 
 	BruteForceDecryptCipher(input1);
 	// correct output, works as intended
-	
 	cout << endl;
 
-	BruteForceDecryptCipher(input2); 
+	BruteForceDecryptCipher(input2);
 	// empty output, because of the if-statement on line 30 
 	// (if the letter is not of the correct type, mark it as a wrong input and return)
+	cout << endl; */
 
-	cout << endl;
+	cout << "Enter an input: " << endl;
+	string inputStr;
+
+	std::getline(cin, inputStr);
+
+	const char* inputAsArr = inputStr.c_str();
+
+	BruteForceDecryptCipher(inputAsArr);
 }
