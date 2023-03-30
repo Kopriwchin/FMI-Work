78a0b98ce7175ccf3d27d3dfc0b66b64af19f968
Kopriwchin <iliqn1234a@gmail.com>
2023-03-30 13:45:35 +0300

    Add more functionality Changed functions' naming type

--- OOP - Second Sem/OOPCoin/OOPCoin/Constants.h
+++ OOP - Second Sem/OOPCoin/OOPCoin/Constants.h
@@ -4,7 +4,8 @@ const short BUFFER_SIZE = 128;
 const char INVALID_FILE_MSG[] = "Error! Could not open requested file!";
 const char* ID_FILE_NAME = "id.txt";
 const char* BLOCKS_FILE_NAME = "blocks.dat";
-
+const char* USERS_FILE_NAME = "users.dat";
+const char* INITUSERS_FILE_NAME = "initUsersFile.dat";
 double ExchangeCoins(const double amount) {
 	const double OOPCOIN_EXCHANGE_RATE = 420;
 	return amount * OOPCOIN_EXCHANGE_RATE;
--- OOP - Second Sem/OOPCoin/OOPCoin/main.cpp
+++ OOP - Second Sem/OOPCoin/OOPCoin/main.cpp
@@ -7,8 +7,10 @@
 using std::cin;
 using std::cout;
 using std::endl;
-using std::ofstream;
+using std::fstream;
 using std::ifstream;
+using std::ofstream;
+
 
 unsigned computeHash(const unsigned char* memory, int length) {
 	unsigned hash = 0xbeaf;
@@ -30,6 +32,8 @@ struct User {
 	char name[128];
 };
 
+User loadedUsers[4];
+
 struct Transaction {
 	unsigned sender;
 	unsigned receiver;
@@ -45,7 +49,7 @@ struct TransactionBlock {
 	Transaction transactions[16];
 };
 
-int AtoI(char* buffer) {
+int aToI(char* buffer) {
 	int result = 0;
 
 	for (int i = 0; buffer[i] != '\0'; i++)
@@ -54,29 +58,35 @@ int AtoI(char* buffer) {
 	return result;
 }
 
-bool CheckIfFileExists(const char* filePath) {
-	ifstream readFile(filePath);
-	if (!readFile.is_open() || !readFile)
-	{
-		readFile.close();
+bool checkIfReadFileExists(std::ifstream &file) {
+	if (!file.is_open() || !file) {
+		file.close();
 		return false;
 	}
-	readFile.close();
 
 	return true;
 }
 
-int InitializeIdFile() {
-	if (CheckIfFileExists(ID_FILE_NAME))
-	{
+bool checkIfWriteFileExists(std::ofstream& file) {
+	if (!file.is_open() || !file) {
+		file.close();
+		return false;
+	}
+
+	return true;
+}
+
+int initializeIdFile() {
+	ifstream idFile(ID_FILE_NAME);
+
+	if (checkIfReadFileExists(idFile)) {
 		// 10 digits in INT type
 		const short BUFFER_SIZE = 10;
 
-		ifstream idFile(ID_FILE_NAME);
 
-		if (!idFile.is_open() || !idFile)
-		{
+		if (!idFile.is_open() || !idFile) {
 			cout << "Invalid file!" << endl;
+			idFile.close();
 			return -1;
 		}
 
@@ -86,7 +96,7 @@ int InitializeIdFile() {
 
 		idFile.close();
 
-		return AtoI(buffer);
+		return aToI(buffer);
 	}
 	else {
 		ofstream idFile(ID_FILE_NAME);
@@ -99,11 +109,10 @@ int InitializeIdFile() {
 	}
 }
 
-bool IncrementId(int id) {
-	ofstream idFile(ID_FILE_NAME, ofstream::out | ofstream::trunc);
+bool incrementId(int id) {
+	ofstream idFile(ID_FILE_NAME, ofstream::trunc);
 
-	if (!idFile.is_open() || !idFile)
-	{
+	if (!idFile.is_open() || !idFile) {
 		cout << INVALID_FILE_MSG << endl;
 		idFile.close();
 		return false;
@@ -116,11 +125,10 @@ bool IncrementId(int id) {
 	return true;
 }
 
-int GetId() {
+unsigned getId() {
 	ifstream idFile(ID_FILE_NAME);
 
-	if (!idFile.is_open() || !idFile)
-	{
+	if (!idFile.is_open() || !idFile) {
 		cout << INVALID_FILE_MSG;
 		idFile.close();
 		return -1;
@@ -132,15 +140,14 @@ int GetId() {
 
 	idFile.close();
 
-	int idResult = AtoI(buffer);
-	IncrementId(idResult);
+	int idResult = aToI(buffer);
+	incrementId(idResult);
 
 	return idResult;
 }
 
-bool IsTransactionValid(Transaction& transaction) {
-	if (transaction.sender == 0)
-	{
+bool isTransactionValid(Transaction& transaction) {
+	if (transaction.sender == 0) {
 		return true;
 	}
 
@@ -160,11 +167,9 @@ bool IsTransactionValid(Transaction& transaction) {
 	return true;
 }
 
-void AddTransactionToBlock(Transaction& transaction) {
-	if (IsTransactionValid(transaction))
-	{
-		if (true)
-		{
+void addTransactionToBlock(Transaction& transaction) {
+	if (isTransactionValid(transaction)) {
+		if (true) {
 
 		}
 	}
@@ -173,13 +178,13 @@ void AddTransactionToBlock(Transaction& transaction) {
 	}
 }
 
-bool CreateUser(const char* username, const double amount) {
-	const unsigned currentUserId = GetId();
+bool createUser(const char* username, const double amount) {
+	const unsigned currentUserId = getId();
 
 	User user = { currentUserId, ""};
 	strcpy_s(user.name, username);
 
-	ofstream writeFile("users.dat", std::ios::binary | std::ios::app);
+	ofstream writeFile(USERS_FILE_NAME, std::ios::binary | std::ios::app);
 
 	if (!writeFile.is_open() || !writeFile)
 	{
@@ -187,21 +192,16 @@ bool CreateUser(const char* username, const double amount) {
 		return false;
 	}
 
-	writeFile.write(reinterpret_cast<const char*>(&user.id), sizeof(int));
-
-	size_t nameLength = strlen(user.name);
-
-	writeFile.write(reinterpret_cast<const char*>(&user.name), nameLength);
-	writeFile.write("\n", 1);
+	writeFile.write(reinterpret_cast<const char*>(&user), sizeof(User));
 
 	writeFile.close();
 
 	Transaction initTransaction { 0, currentUserId, ExchangeCoins(amount), std::time(nullptr) };
-	AddTransactionToBlock(initTransaction);
+	addTransactionToBlock(initTransaction);
 	return true;
 }
 
-double GetUserAvailableCoins(unsigned id) {
+double getUserAvailableCoins(unsigned id) {
 	ifstream blocksFile("blocks.dat", std::ios::binary);
 
 	return 0;
@@ -215,21 +215,146 @@ double GetUserAvailableCoins(unsigned id) {
 //	// TODO
 //}
 
-void InitializeBlockFile() {
+void initializeBlockFile() {
+	ofstream writeFile(BLOCKS_FILE_NAME, std::ios::binary);
 
-	if (!CheckIfFileExists(BLOCKS_FILE_NAME))
+	if (!checkIfWriteFileExists(writeFile))
 	{
-		ofstream writeFile(BLOCKS_FILE_NAME, std::ios::binary);
 
 		writeFile.close();
 	}
 }
+int getCountUsers()
+{
+	ifstream readFile(USERS_FILE_NAME, std::ios::binary);
+
+	if (!checkIfReadFileExists(readFile))
+	{
+		return -1;
+	}
+	readFile.seekg(0, std::ios::end);
+	int usersCount = readFile.tellg() / sizeof(User);
+
+	readFile.close();
+
+	return usersCount;
+}
+
+//User* loadUsers() {
+//	int usersCount = countUsers();
+//	ifstream readFile(USERS_FILE_NAME, std::ios::binary);
+//
+//	User* users = new User[usersCount];
+//
+//	if (usersCount != -1)
+//	{
+//		for (int i = 0; i < usersCount; i++)
+//		{
+//			User initUser;
+//			readFile.read(reinterpret_cast<char*>(&initUser), sizeof(User));
+//			users[i] = initUser;
+//			cout << loadedUsers[i].id << endl;
+//			cout << loadedUsers[i].name << endl;
+//		}
+//
+//	}
+//
+//	readFile.close();
+//
+//	return users;
+//}
+
+//bool findUserByName(char* username) {
+//	ifstream usersFile("users.bat", std::ios::binary);
+//	User user;
+//	while (!usersFile.eof())
+//	{
+//		usersFile.read(reinterpret_cast<char*>(&user), sizeof(User));
+//		if (true)
+//		{
+//
+//		}
+//	}
+//}
+	
+//User findUserById(unsigned id) {
+//
+//}
+
+void removeUser(char* username) {
+	ifstream readFile(USERS_FILE_NAME, std::ios::binary);
+	int countUsers = getCountUsers();
+	bool isUserFound = false;
+
+	if (countUsers != -1) {
+		ofstream writeFile(INITUSERS_FILE_NAME, std::ios::binary);
+
+		for (int i = 1; i <= countUsers; i++) {
+			User initUser;
+			readFile.read(reinterpret_cast<char*>(&initUser), sizeof(User));
+
+			// Copy every user into another file, excluding the user, that we want to delete
+			if (strcmp(initUser.name, username) != 0) {
+				writeFile.write(reinterpret_cast<const char*>(&initUser), sizeof(User));
+			}
+			else {
+				isUserFound = true;
+			}
+		}
+
+		// Delete users.bat file and renaming the initial file to users.bat
+		if (isUserFound) {
+			readFile.close();
+			writeFile.close();
+
+			if (remove(USERS_FILE_NAME)) {
+				cout << "Error deleting!" << endl; 
+				return;
+			}
+
+			if (std::rename(INITUSERS_FILE_NAME, USERS_FILE_NAME)) {
+				cout << "Error renaming!" << endl;
+				return;
+			}
+		}
+		else {
+			cout << "User does not exist!" << endl;
+		}
+	}
+}
+
 
 //void TransferCoins(const unsigned senderId, const unsigned receiverId) {
 //	
 //}
 
-void PrintProgramCommands()
+void validateAmount(double& amount) {
+	while (true)
+	{
+		if (amount < 0)
+		{
+			cout << "Invalid input! Amount must be a positive number!" << endl;
+			cin >> amount;
+			continue;
+		}
+		break;
+	}
+}
+
+void validateName(char* name) {
+	while (true)
+	{
+		if (strlen(name) < 4 || strlen(name) > 126)
+		{
+			cout << "Invalid input! Name length must be more between 4 and 126 characters!" << endl;
+			cin >> name;
+			continue;
+		}
+		break;
+	}
+}
+
+void printProgramCommands()
 {
 	cout << '\n';
 	cout << "Exit program -> 0" << endl;
@@ -244,10 +369,9 @@ void PrintProgramCommands()
 int main() {
 	cout << "Welcome to OOP-Coin!" << endl;
 
-	PrintProgramCommands();
+	printProgramCommands();
 	
-	InitializeIdFile();
-	InitializeBlockFile();
+	initializeIdFile();
 
 	int chosenCommand;
 
@@ -263,52 +387,51 @@ int main() {
 			case 1:
 				cout << "Enter username: ";
 				cin >> username;
-				ValidateName(username);
+				validateName(username);
 
 				cout << "Enter amount: ";
 				cin >> amount;
-				ValidateAmount(amount);
-				cout << amount;
+				validateAmount(amount);
 
-				CreateUser(username, amount);
+				createUser(username, amount);
 				break;
 			case 2:
 				cout << "Enter username: ";
 				cin >> username;
-				ValidateName(username);
+				validateName(username);
 
-				// RemoveUser(username);
+				removeUser(username);
 				break;
 			case 3:
 				cout << "Enter sender's username: ";
 				char senderName[128];
 				cin >> senderName;
-				ValidateName(senderName);
+				validateName(senderName);
 
 				cout << "Enter receiver's name: ";
 				char receiverName[128];
 				cin >> receiverName;
-				ValidateName(receiverName);
+				validateName(receiverName);
 				
 				// SendCoins(senderName, receiverName, amount);
 				break;
 			case 4:
 				cout << "Enter amount of users shown: ";
 				cin >> amount;
-				ValidateAmount(amount);
+				validateAmount(amount);
 				//PrintWealthiestUsers(int amount);
 				break;
 			case 5:
 				cout << "Enter amount of users shown: ";
 				cin >> amount;
-				ValidateAmount(amount);
+				validateAmount(amount);
 				//PrintBiggestBlocks(int amount);
 				break;
 			default:
 				cout << "Invalid command!" << endl;
 				break;
 		}
-		PrintProgramCommands();
+		printProgramCommands();
 		cin >> chosenCommand;
 	}
 
